858ed8cc48fc48899de1de9768834f9b4bc1ff43
yvour <ivour@rambler.ru>
2023-08-06 22:19:25 +0200

    Adding first test

--- src/utils/add.test.ts
+++ src/utils/add.test.ts
@@ -1,7 +1,21 @@
 import { addMatch } from './add'
 
+const TIME_COST = 'TIME_COST'
+
 describe('addMatch', () => {
-    it('should add an item', () => {
-        expect(addMatch([], {})).toEqual([{}])
+    it('should add an item to empty array', () => {
+        expect(
+            addMatch([], {
+                homeTeam: { name: 'Land One', score: 0 },
+                awayTeam: { name: 'Land Two', score: 0 },
+            })
+        ).toEqual([
+            {
+                homeTeam: { name: 'Land One', score: 0 },
+                awayTeam: { name: 'Land Two', score: 0 },
+                id: 'Land One - Land Two',
+                startTime: TIME_COST,
+            },
+        ])
     })
 })
