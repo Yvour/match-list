ea1d4677d1ef43e76758b2d613d518efdb0c9fc6
yvour <ivour@rambler.ru>
2023-08-06 22:43:26 +0200

    Adding tests

--- src/utils/create.test.ts
+++ src/utils/create.test.ts
@@ -23,4 +23,50 @@ describe('createMatch', () => {
       },
     ])
   })
+
+  it('should add an item to filled array', () => {
+    expect(
+      createMatch(
+        [
+          {
+            id: 'first id',
+            startTime: 'first time',
+            homeTeam: { name: 'First Land One', score: 0 },
+            awayTeam: { name: 'First Land Two', score: 0 },
+          },
+
+          {
+            id: 'last id',
+            startTime: 'last time',
+            homeTeam: { name: 'Last Land One', score: 0 },
+            awayTeam: { name: 'Last Land Two', score: 0 },
+          },
+        ],
+        {
+          homeTeam: { name: 'Land One', score: 0 },
+          awayTeam: { name: 'Land Two', score: 0 },
+        }
+      )
+    ).toEqual([
+      {
+        homeTeam: { name: 'Land One', score: 0 },
+        awayTeam: { name: 'Land Two', score: 0 },
+        id: 'Land One - Land Two',
+        startTime: TIME_COST,
+      },
+      {
+        id: 'first id',
+        startTime: 'first time',
+        homeTeam: { name: 'First Land One', score: 0 },
+        awayTeam: { name: 'First Land Two', score: 0 },
+      },
+
+      {
+        id: 'last id',
+        startTime: 'last time',
+        homeTeam: { name: 'Last Land One', score: 0 },
+        awayTeam: { name: 'Last Land Two', score: 0 },
+      },
+    ])
+  })
 })
--- src/utils/update.test.ts
+++ src/utils/update.test.ts
@@ -28,6 +28,57 @@ describe('updateMatch', () => {
     ])
   })
 
+  it('should update an item by id in filled array', () => {
+    expect(
+      updateMatch(
+        [
+          {
+            id: 'first id',
+            startTime: 'first time',
+            homeTeam: { name: 'First Land One', score: 0 },
+            awayTeam: { name: 'First Land Two', score: 0 },
+          },
+          {
+            id: 'some id',
+            startTime: 'some time',
+            homeTeam: { name: 'Land One', score: 0 },
+            awayTeam: { name: 'Land Two', score: 0 },
+          },
+          {
+            id: 'last id',
+            startTime: 'last time',
+            homeTeam: { name: 'Last Land One', score: 0 },
+            awayTeam: { name: 'Last Land Two', score: 0 },
+          },
+        ],
+        {
+          homeTeam: { name: 'Land One', score: 0 },
+          awayTeam: { name: 'Land Two', score: 1 },
+        },
+        'some id'
+      )
+    ).toEqual([
+      {
+        id: 'first id',
+        startTime: 'first time',
+        homeTeam: { name: 'First Land One', score: 0 },
+        awayTeam: { name: 'First Land Two', score: 0 },
+      },
+      {
+        homeTeam: { name: 'Land One', score: 0 },
+        awayTeam: { name: 'Land Two', score: 1 },
+        id: 'some id',
+        startTime: 'some time',
+      },
+      {
+        id: 'last id',
+        startTime: 'last time',
+        homeTeam: { name: 'Last Land One', score: 0 },
+        awayTeam: { name: 'Last Land Two', score: 0 },
+      },
+    ])
+  })
+
   it('should return an equal array if id is not actual', () => {
     const initArray = [
       {
