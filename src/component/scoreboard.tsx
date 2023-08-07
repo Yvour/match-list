90ed96732f8b2d0d1f463d6a8d69ddea563dc0dc
yvour <ivour@rambler.ru>
2023-08-07 15:21:39 +0200

    Adding Update Functionality for Scoreboard

--- src/component/scoreboard.tsx
+++ src/component/scoreboard.tsx
@@ -3,12 +3,17 @@ import { IMatchListEntry } from '../types/types';
 import { MatchTable } from './match-table';
 import { createMatch } from '../utils/create';
 import { AddingForm } from './adding-form';
+import { UpdatingForm } from './updating-form';
+import { updateMatch } from '../utils/update';
 
 interface IScoreboardProps {
   initialList: IMatchListEntry[];
 }
 
 export const Scoreboard = (props: IScoreboardProps) => {
+  const [isUpdating, setIsUpdating] = React.useState(false);
+  const [editFormEntry, setEditFormEntry] =
+    React.useState<IMatchListEntry | null>(null);
   const [isAdding, setIsAdding] = React.useState(false);
   const [list, setList] = React.useState(
     Array.isArray(props.initialList) ? props.initialList : []
@@ -29,14 +34,34 @@ export const Scoreboard = (props: IScoreboardProps) => {
     setIsAdding(false);
   };
   const showAddingForm = () => setIsAdding(true);
+  const onUpdateButtonClick = (item: IMatchListEntry) => {
+    setEditFormEntry(item);
+    setIsUpdating(true);
+  };
+  const onUpdateCancel = () => {
+    setEditFormEntry(null);
+    setIsUpdating(false);
+  };
+  const onUpdateFormSubmit = (updatedItem: IMatchListEntry) => {
+    const updatedList = updateMatch(list, updatedItem, updatedItem.id);
+    setList(updatedList);
+    setIsUpdating(false);
+  };
 
   return (
     <div>
       {isAdding ? (
         <AddingForm onCancel={closeAddingForm} onSubmit={submitAddingForm} />
       ) : null}
+      {isUpdating ? (
+        <UpdatingForm
+          onCancel={onUpdateCancel}
+          onSubmit={onUpdateFormSubmit}
+          matchEntry={editFormEntry}
+        />
+      ) : null}
       <button onClick={showAddingForm}>Add</button>
-      <MatchTable list={list} />
+      <MatchTable list={list} onUpdateButtonClick={onUpdateButtonClick} />
     </div>
   );
 };
