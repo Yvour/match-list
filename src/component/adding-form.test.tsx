f24fa8d05f0a3875f6c20fd5b4efc5aa7d06589f
yvour <ivour@rambler.ru>
2023-08-07 09:57:51 +0200

    Adding test for `Adding form` + dummy implementation

---
+++ src/component/adding-form.test.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { AddingForm } from './adding-form';
+
+import { fireEvent, render, screen } from '@testing-library/react';
+
+const mockedOnSubmit = jest.fn();
+const mockedOnCancel = jest.fn();
+
+describe('AddingForm', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should render two inputs and two buttons', () => {
+    <AddingForm onSubmit={mockedOnSubmit} />;
+
+    expect(
+      screen.getByLabelText('Home Team', { selector: 'input' })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByLabelText('Away Team', { selector: 'input' })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+  });
+});
---
+++ src/component/adding-form.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { IMatchListEntry } from '../types/types';
+
+interface Interface {
+  onSubmit: (awayTeamName: string, homeTeamName: string) => void;
+  onCancel: () => void;
+}
+
+export const AddingForm = (props: IAddingFormProps) => {
+  return null;
+};
