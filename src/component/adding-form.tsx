151521426d423c4c34aac03f1622b7221b569b26
yvour <ivour@rambler.ru>
2023-08-07 10:52:42 +0200

    Adding implementation for submitting values

--- src/component/adding-form.tsx
+++ src/component/adding-form.tsx
@@ -5,19 +5,34 @@ interface Interface {
   onCancel: () => void;
 }
 
-export const AddingForm = ({ onCancel }: IAddingFormProps) => {
+export const AddingForm = ({ onCancel, onSubmit }: IAddingFormProps) => {
+  const [homeTeamName, setHomeTeamName] = React.useState('');
+  const [awayTeamName, setAwayTeamName] = React.useState('');
+
+  const onSubmitButtonClick = () => {
+    onSubmit({ homeTeamName, awayTeamName });
+  };
+
   return (
     <div>
       <label>
         Home Team
-        <input aria-label="Home Team" type="text" />
+        <input
+          type="text"
+          value={homeTeamName}
+          onChange={(e) => setHomeTeamName(e.target.value)}
+        />
       </label>
       <label>
         Away Team
-        <input type="text" />
+        <input
+          type="text"
+          value={awayTeamName}
+          onChange={(e) => setAwayTeamName(e.target.value)}
+        />
       </label>
       <button onClick={onCancel}>Cancel</button>
-      <button>Submit</button>
+      <button onClick={onSubmitButtonClick}>Submit</button>
     </div>
   );
 };
