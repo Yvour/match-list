66a69c02a8d923623030f074adc9fbdeae994da1
yvour <ivour@rambler.ru>
2023-08-07 09:40:29 +0200

    Adding implementation for the first test

--- src/component/scoreboard.tsx
+++ src/component/scoreboard.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { IMatchListEntry } from '../types/types';
+import {MatchTable} from "./match-table";
 
 interface IScoreboardProps {
   initialList: IMatchListEntry[];
 }
 
-export const Scoreboard = () => {
-  return null;
+export const Scoreboard = (props:IScoreboardProps) => {
+  const [list, setList] = React.useState(Array.isArray(props.initialList)? props.initialList : [])
+
+  return <div>
+    <button>Add</button>
+    <MatchTable list={list} />
+  </div>;
 };
