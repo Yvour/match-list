d5180ea49ae2aa857ceaac5c6f891abb0d00dc18
yvour <ivour@rambler.ru>
2023-08-07 09:53:59 +0200

    prettify

--- src/component/scoreboard.tsx
+++ src/component/scoreboard.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import { IMatchListEntry } from '../types/types';
-import {MatchTable} from "./match-table";
+import { MatchTable } from './match-table';
 
 interface IScoreboardProps {
   initialList: IMatchListEntry[];
 }
 
-export const Scoreboard = (props:IScoreboardProps) => {
-  const [list, setList] = React.useState(Array.isArray(props.initialList)? props.initialList : [])
+export const Scoreboard = (props: IScoreboardProps) => {
+  const [list, setList] = React.useState(
+    Array.isArray(props.initialList) ? props.initialList : []
+  );
 
-  return <div>
-    <button>Add</button>
-    <MatchTable list={list} />
-  </div>;
+  return (
+    <div>
+      <button>Add</button>
+      <MatchTable list={list} />
+    </div>
+  );
 };
